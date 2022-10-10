d6257dd595828cc29de24fb8e2357a855b7bfeb5
Roman <romanshliakhov@gmail.com>
2022-10-10 23:19:32 +0300

    add slider

--- source/js/components/sliders.js
+++ source/js/components/sliders.js
@@ -1,29 +1,29 @@
 import Swiper from '../vendor/swiper.js';
 
-// Pets slider
-let sliderBestConfectioner = new Swiper(".pets__slider", {
-  slidesPerView: 'auto',
-  spaceBetween: 2,
-  observer: true,
-  freeMode: true,
-  loop: true,
-  navigation: {
-      nextEl: ".pets__slider-arrow--next",
-      prevEl: ".pets__slider-arrow--prev",
+for (const container of document.querySelectorAll('.container') ) {
+  let matSlider = new Swiper(container.querySelector('.mat__slider'), {
+    slidesPerView: 1,
+    freeMode: true,
+    loop: true,
+    navigation: {
+      nextEl: container.querySelector(".slider__btn--next"),
+      prevEl: container.querySelector(".slider__btn--prev"),
       clickable: true,
   },
-  // breakpoints: {
-  //   320: {
-  //       slidesPerView: 'auto',
-  //       freeMode: true,
-  //       spaceBetween: 10,
-  //     },
-  //   1440: {
-  //     slidesPerView: 3,
-  //     spaceBetween: 23.5,
-  //   }
-  // }
-});
+    // breakpoints: {
+    //   320: {
+    //       slidesPerView: 'auto',
+    //       freeMode: true,
+    //       spaceBetween: 10,
+    //     },
+    //   1440: {
+    //     slidesPerView: 5,
+    //     spaceBetween: 18,
+    //   }
+    // }
+  });
+}
+
 
 
 
