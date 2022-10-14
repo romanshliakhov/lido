29acb68d5e4a0a1c1bcd7902265d191a6f542f4e
Roman <romanshliakhov@gmail.com>
2022-10-15 01:14:07 +0300

    add modal

--- source/js/components/sliders.js
+++ source/js/components/sliders.js
@@ -3,7 +3,7 @@ import Swiper from '../vendor/swiper.js';
 for (const container of document.querySelectorAll('.container') ) {
   let matSlider = new Swiper(container.querySelector('.mat__slider'), {
     slidesPerView: 1,
-    freeMode: true,
+    // freeMode: true,
     loop: true,
     pagination: {
       el: ".swiper-pagination",
