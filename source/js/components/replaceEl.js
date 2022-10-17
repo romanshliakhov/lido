e04c2776e3f7865a015808e2e56e3bc87b62b94a
Roman <romanshliakhov@gmail.com>
2022-10-17 14:13:29 +0300

    fix v3

--- source/js/components/replaceEl.js
+++ source/js/components/replaceEl.js
@@ -1,45 +1,45 @@
-let replaceSettings = {
-  beforebegin: 'beforebegin', // перед самим элементом targetElement
-  afterbegin: 'afterbegin', // внутри элемента targetElement, перед его первым потомком
-  beforeend: 'beforeend', //  внутри элемента targetElement, после его последнего потомка
-  afterend: 'afterend', // после самого элемента targetElement
-  mobile: 576,
-  smallTablet: 768,
-  tablet: 1024
-}
+// let replaceSettings = {
+//   beforebegin: 'beforebegin', // перед самим элементом targetElement
+//   afterbegin: 'afterbegin', // внутри элемента targetElement, перед его первым потомком
+//   beforeend: 'beforeend', //  внутри элемента targetElement, после его последнего потомка
+//   afterend: 'afterend', // после самого элемента targetElement
+//   mobile: 576,
+//   smallTablet: 768,
+//   tablet: 1024
+// }
 
-let elementName = {
-  sortingBtn : '.mat__gift',
-  formParentBtn: '.mat__information',
-  mobileSiblingstBtn: '.mat__consultation',
-}
+// let elementName = {
+//   sortingBtn : '.mat__gift',
+//   formParentBtn: '.mat__information',
+//   mobileSiblingstBtn: '.mat__consultation',
+// }
 
 // let items = [...document.querySelectorAll('.mat__wrapp')];
 
-const replaceElements = (elements, elementClass, desktopClass, mobileClass, mobileSetting, desktopSetting, breakpointSetting) => {
-  let containerWidth = document.documentElement.clientWidth;
+// const replaceElements = (elements, elementClass, desktopClass, mobileClass, mobileSetting, desktopSetting, breakpointSetting) => {
+//   let containerWidth = document.documentElement.clientWidth;
 
-  elements.map(function (item) {
-    const myElement = item.querySelector(elementClass);
-    const myDesktop = item.querySelector(desktopClass);
-    const myMobile = item.querySelector(mobileClass);
+//   elements.map(function (item) {
+//     const myElement = item.querySelector(elementClass);
+//     const myDesktop = item.querySelector(desktopClass);
+//     const myMobile = item.querySelector(mobileClass);
 
-    (function() {
-      if (myDesktop && myElement && myMobile) {
-        containerWidth <= breakpointSetting ?
-        myMobile.insertAdjacentElement(mobileSetting, myElement) :
-        myDesktop.insertAdjacentElement(desktopSetting, myElement);
-      }
-    }());
-  });
-}
+//     (function() {
+//       if (myDesktop && myElement && myMobile) {
+//         containerWidth <= breakpointSetting ?
+//         myMobile.insertAdjacentElement(mobileSetting, myElement) :
+//         myDesktop.insertAdjacentElement(desktopSetting, myElement);
+//       }
+//     }());
+//   });
+// }
 
-window.addEventListener('resize', () => {
-  replaceElements(items, elementName.sortingBtn, elementName.formParentBtn, elementName.mobileSiblingstBtn, replaceSettings.afterbegin, replaceSettings.afterend, replaceSettings.tablet);
+// window.addEventListener('resize', () => {
+//   replaceElements(items, elementName.sortingBtn, elementName.formParentBtn, elementName.mobileSiblingstBtn, replaceSettings.afterbegin, replaceSettings.afterend, replaceSettings.tablet);
 
-});
+// });
 
-window.addEventListener('DOMContentLoaded', () => {
-  replaceElements(items, elementName.sortingBtn, elementName.formParentBtn, elementName.mobileSiblingstBtn, replaceSettings.afterbegin, replaceSettings.afterend, replaceSettings.tablet);
+// window.addEventListener('DOMContentLoaded', () => {
+//   replaceElements(items, elementName.sortingBtn, elementName.formParentBtn, elementName.mobileSiblingstBtn, replaceSettings.afterbegin, replaceSettings.afterend, replaceSettings.tablet);
 
-});
+// });
