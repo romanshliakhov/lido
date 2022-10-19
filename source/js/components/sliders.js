c5af93ad2154e70bb08dc0ee057ad0bdabde0031
Roman <romanshliakhov@gmail.com>
2022-10-19 19:17:01 +0300

    try to fix slider

--- source/js/components/sliders.js
+++ source/js/components/sliders.js
@@ -1,5 +1,30 @@
 import Swiper from '../vendor/swiper.js';
 
+let giftsSlider = new Swiper(".gifts__slider", {
+  slidesPerView: 1.75,
+  spaceBetween: 12,
+  centeredSlides: true,
+  loop: true,
+  observer: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 1.75,
+      spaceBetween: 12,
+      },
+    768: {
+      slidesPerView: 2.75,
+      spaceBetween: 15,
+      centeredSlides: true,
+      },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+      centeredSlides: false,
+      observer: true,
+    }
+  }
+});
+
 for (const container of document.querySelectorAll('.container') ) {
   let matSlider = new Swiper(container.querySelector('.mat__slider'), {
     slidesPerView: 1,
@@ -17,31 +42,9 @@ for (const container of document.querySelectorAll('.container') ) {
   });
 };
 
-let giftsSlider = new Swiper(".gifts__slider", {
-  slidesPerView: 1.75,
-  spaceBetween: 12,
-  centeredSlides: true,
-  loop: true,
-  observer: true,
-  breakpoints: {
-    320: {
-      slidesPerView: 1.75,
-      spaceBetween: 12,
-      observer: true,
-      },
-    768: {
-      slidesPerView: 3.25,
-      spaceBetween: 15,
-      centeredSlides: true,
-      },
-    1024: {
-      slidesPerView: 4,
-      spaceBetween: 20,
-      centeredSlides: false,
-      observer: true,
-    }
-}
-});
+
+
+
 
 
 
