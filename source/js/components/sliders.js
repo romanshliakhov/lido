7ee2f4b80c6bb6fbee5d6a97a03fff32aea50995
Roman <romanshliakhov@gmail.com>
2022-10-19 19:27:58 +0300

    add gifts slider height

--- source/js/components/sliders.js
+++ source/js/components/sliders.js
@@ -1,5 +1,22 @@
 import Swiper from '../vendor/swiper.js';
 
+for (const container of document.querySelectorAll('.container') ) {
+  let matSlider = new Swiper(container.querySelector('.mat__slider'), {
+    slidesPerView: 1,
+    loop: true,
+    observer: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: container.querySelector(".mat__slider-btn--next"),
+      prevEl: container.querySelector(".mat__slider-btn--prev"),
+      clickable: true,
+    },
+  });
+};
+
 let giftsSlider = new Swiper(".gifts__slider", {
   slidesPerView: 1.75,
   spaceBetween: 12,
@@ -25,22 +42,6 @@ let giftsSlider = new Swiper(".gifts__slider", {
   }
 });
 
-for (const container of document.querySelectorAll('.container') ) {
-  let matSlider = new Swiper(container.querySelector('.mat__slider'), {
-    slidesPerView: 1,
-    loop: true,
-    observer: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: container.querySelector(".mat__slider-btn--next"),
-      prevEl: container.querySelector(".mat__slider-btn--prev"),
-      clickable: true,
-    },
-  });
-};
 
 
 
